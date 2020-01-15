1c11e1adb27fb16011d74e5952629c2ad47a6bf5
D. Zlatkov <d.s.zlatkov@gmail.com>
2020-01-15 17:07:08 +0200

    added Routes, Home, NotFound containers.

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Worktabl</h1>
+        <p>The platform that grows companies</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
